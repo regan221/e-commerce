48412ffd4a25b6c862734c7cb7ae5f1cf2b807ea
Regan <conchadaregan221@gmail.com>
2021-07-24 21:43:53 +0800

    new-git-conflict

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,6 +20,9 @@
                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
+                <span>
+                    <h1>git-conflict-here</h1>
+                </span>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
         </div>
