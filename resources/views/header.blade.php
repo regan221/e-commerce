ab5b2675ae4c92e073672ba4b7fa753c942c851d
Regan <conchadaregan221@gmail.com>
2021-07-24 18:43:29 +0800

    changed some of the ui for login page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,6 +12,20 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Orders</a>
         </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+        </li>
+
+
         {{-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Dropdown
@@ -27,10 +41,7 @@
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li> --}}
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      <div class="dropdown-divider"></div>
       <ul class="navbar-nav">
             <li class="nav-item">
             <a class="nav-link" href="#">Login <span class="sr-only"></span></a>
@@ -39,5 +50,9 @@
             <a class="nav-link" href="#">Signup</a>
             </li>
       </ul>
+      {{-- <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+      </form> --}}
     </div>
   </nav>
