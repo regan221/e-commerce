824efbb1df4fddcc931b75fec4ddaa276bdbaebd
Regan <conchadaregan221@gmail.com>
2021-07-24 21:42:36 +0800

    git conflict

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,6 +20,9 @@
                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
+                <div>
+                    git conflict
+                </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
         </div>
