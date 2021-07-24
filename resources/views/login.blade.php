a8f533ea3b3ef1f4224fefd8bdcbca23d9dec55e
Regan <conchadaregan221@gmail.com>
2021-07-24 21:58:59 +0800

    Overwrite all the changes on the other branch

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
 <div class="container p-5 mt-5">
     <div class="row d-flex">
         <div class="col-sm-4 my-auto mx-auto">
-            <form class="py-5 px-3">
+            <form class="shadow py-5 px-3">
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email address</label>
                   <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -16,23 +16,9 @@
                   <label for="exampleInputPassword1">Password</label>
                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-        </div>
-
-        <div class="col-sm-4 my-auto mx-auto">
-            <form class="py-5 px-3">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div>
-                    git conflict
+                <div class="form-group form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
